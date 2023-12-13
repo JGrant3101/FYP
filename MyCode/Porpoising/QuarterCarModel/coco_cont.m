7ff0d416ce2a622ca2f8ae8cd780aa656e443b24
JGrant3101 <joegrant01423@gmail.com>
2023-12-13 17:46:01 +0000

    Adapting to work with ep toolbox
    Making some tweaks to the Suspension functions as well as writing the functions that return the Jacobians for it w.r.t x and p vectors and updating the coco_cont script to run

--- MyCode/Porpoising/QuarterCarModel/coco_cont.m
+++ MyCode/Porpoising/QuarterCarModel/coco_cont.m
@@ -27,9 +27,34 @@ Inputs(10, 1) = 0.07;
 % Scaling applied to Inverse Gaussian distribution
 Inputs(11, 1) = 0.1*(500/9)^2;
 
-u0 = [-31.1e-3;-8.4e-3;0;0] ;
+% Defining out initial x array
+x0 = [-31.1e-3;-8.4e-3;0;0];
+
+% Defining the names of the inputs already written above
+pnames = {'Ms' 'Mu' 'Ks' 'Cs' 'Kt' 'H' 'vCar' 'A' 'mew' 'lamda' 'scaling'};
+
+% Initialising the COCO problem
 prob = coco_prob();
 
-bd1 = coco(prob, 'car/4', 'ode', 'isol', 'ep', ...
-  @(u,p)Suspension(0,u,p), [], [], u0, {'al' 'be' 'de' 'ro'}, Inputs,  ...               % ep toolbox arguments
-  1, {'be' 'ep.test.SN' 'ep.test.HB' 'ep.test.USTAB' 'atlas.test.FP'}, [2 7]); % cont toolbox arguments
+% Defining the functions
+funcs = {@Suspension, @Suspension_dx, @Suspension_dp};
+
+% Defining the function arguments
+args = {funcs{:}, x0, pnames, Inputs};
+
+% Defining the arguments we want COCO to vary and in what range we want
+% them varied
+variableargs = {2, {'Cs', 'vCar'}, [2000 5000; 100 340]};
+
+% Running COCO
+bd1 = coco(prob, 'Test1', @ode_isol2ep, args{:}, variableargs{:});
+
+%bd1 = coco(prob, 'car', 'ode', 'isol', 'ep', ...
+ % @(u,p)Suspension(u,p), [], [], u0, {'al' 'be' 'de' 'ro'}, Inputs,  ...               % ep toolbox arguments
+  %1, {'be' 'ep.test.SN' 'ep.test.HB' 'ep.test.USTAB' 'atlas.test.FP'}, [2 7]); % cont toolbox arguments
+
+
+
+
+
+
